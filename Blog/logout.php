d95167bc9eccfc4860b56cfd3e53c55828190822
arifnawfas-png <arifnawfas@gmail.com>
2026-01-04 13:00:23 +0800

    Delete Blog directory

--- Blog/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once 'db.php';
-
-session_start();
-logout();
-redirectAndExit('index.php');
-?>
--- Blog/templates/head.php
+++
@@ -1,3 +0,0 @@
-
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="assets/main.css" />
